1e886bc19829895faeeed994604c3c4a5dac4469
Adarsh ML <mladarsh08@gmail.com>
2021-05-18 17:01:14 +0530

    Contact us page done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import Landing from "./components/Landing";
 import Vision from "./components/Vision";
 import Team from "./components/Team";
 import ContactUs from "./components/ContactUs";
+import styled from "styled-components";
+
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Landing />
       <Vision />
       <Team />
       <ContactUs />
-    </div>
+    </AppContainer>
   );
 }
+const AppContainer = styled.div`
+    width:100%;
+  
+    text-align: center;
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+`;
+
+
+
 
 export default App;
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,12 +1,43 @@
+import styled from 'styled-components';
 const ContactUs = () => {
     return (
-        <div>
-            <h4>And we've got the best team to do it.</h4>
-            <h1>HIT US UP</h1>
-            <a href="">LinkedIn</a>
-            <a href="">Email</a>
-        </div>
+        <Page>
+            <CuriousText>Interested in our tech? or just curious?</CuriousText>
+            <HitUsText>HIT US UP</HitUsText>
+            <LinksText>
+                <a href="">LinkedIn</a>
+                <a href="">Email</a>
+            </LinksText>
+        </Page>
     )
 }
+
+const Page = styled.div`
+    width:100%;
+    height:100vh;
+    flex-wrap: wrap;
+    flex-direction: column;
+    background-color:white;
+    justify-content: center;
+    color: black;
+    gap: 0px 0%;
+`;
+
+const CuriousText = styled.p`
+    font-size:3vw;
+    border:2px red solid;
+`;
+const HitUsText = styled.p`
+    border:2px red solid;
+    font-size:12vw;
+`;
+const LinksText = styled.h2`
+    border:2px red solid;
+    display:flex;
+    flex-direction: column;
+`;
+const EmptySpace = styled.div`
+    flex-basis:10%;
+`
 
 export default ContactUs
