ccb336ac6051fcb6da426c296f691a8053e9d86e
Adarsh ML <mladarsh08@gmail.com>
2021-05-18 13:42:42 +0530

    Add lottie and image assets. close out on Landing, Vision and Team pages. Add lottie dependencya nd styled-components dependency. Next up Contact us, typography and L2 pages

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,59 @@
+import styled from "styled-components";
+import Arrow from "../assets/arrow.svg";
+import TeamImage from "../assets/team.svg";
 const Team = () => {
     return (
-        <div>
-            <h1>And we've got the best team to do it.</h1>
-        </div>
+        <Page>
+         
+                <TextContainer>
+                    <Text>And we've got the best team to do it.</Text>
+                    <ArrowContainer>
+                        <img src={Arrow} style={{ height: '8vw',width:'8vw', maxWidth:'100px' }}  />
+                    </ArrowContainer>
+                </TextContainer>
+                <ImageContainer >
+                    <img src={TeamImage}  style={{ height: '50vw',width:'50vw', maxWidth:'500px' }} />
+                </ImageContainer>
+        </Page>
     )
 }
+
+
+const Page = styled.div`
+margin:auto;
+    width:100%;
+    height:100vh;
+    background-color:black;
+    color: white;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-around;
+    align-items: auto;    
+`;
+
+const TextContainer = styled.div`
+    display:flex;
+    flex-direction:row;
+    
+`;
+const ArrowContainer = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+`;
+const ImageContainer = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:flex-end;
+    flex-basis: 40%;
+
+`;
+
+
+const Text = styled.h2`
+    font-size: clamp(70px, 7vw, 22px);
+    align-self: flex-start;
+    flex-basis:50%;
+`;
 
 export default Team
--- src/components/Vision.js
+++ src/components/Vision.js
@@ -1,11 +1,55 @@
+import styled from "styled-components";
+import Lottie from 'react-lottie';
+import animationData from '../lotties/freedom-grid.json';
 const Vision = () => {
     const firstText = "Imagine all the awesomeness of going electric without the shackles of charging."
     const secondText = "Through Exponent Enabled rapid charging tech we want to keep you going Faster. Farther. Free-er"
     return (
-        <div>
-            <h2>{firstText}</h2>
-        </div>
+        <Page>
+            <Text>{firstText}</Text>
+            <div>
+                <Lottie 
+                    options={defaultOptions}
+                    height='100%'
+                    width='100%'
+                />
+            </div>
+        </Page>
     )
 }
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+const Page = styled.div`
+    width:100%;
+    height:100vh;
+    text-align: center;
+    display:flex;
+    flex-direction: column;
+    flex-wrap:wrap;
+    justify-content:center;
+    align-content:center;
+    background-color:white;
+    color: white;
+`;
+
+const Text = styled.h2`
+    color:black;
+    font-size:2em;
+    flex-basis:20%;
+`;
+
+const LottieContainer = styled.div`
+    width:100%;
+    height:100%;
+    flex-basis:40%;
+`;
 
 export default Vision
